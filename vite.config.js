f0ecb2b02bf8ddb981ed40f3bdcea98e5a165d59
gladja <gladkih.alexander@gmail.com>
2024-01-04 20:07:01 +0200

    refactor

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tobi-vue3/',
+  // base: '/tobi-vue3/',
   plugins: [vue()],
   resolve: {
     alias: {
