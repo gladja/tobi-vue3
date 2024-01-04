221281b33055b917d08dce8738fa9d451ee03a79
gladja <gladkih.alexander@gmail.com>
2024-01-04 19:07:08 +0200

    refactor vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import glob from 'glob'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import injectHTML from 'vite-plugin-html-inject'
+import FullReload from 'vite-plugin-full-reload'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  root: 'src',
+  plugins: [vue(), injectHTML(), FullReload(['./src/**/**.html'])],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
