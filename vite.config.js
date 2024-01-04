cf170dd9bc348064bd036df4c520f9f890faa9dc
gladja <gladkih.alexander@gmail.com>
2024-01-04 19:54:57 +0200

    refactor

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-import glob from 'glob'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// import injectHTML from 'vite-plugin-html-inject'
-// import FullReload from 'vite-plugin-full-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,10 +12,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-  // build: {
-  //   rollupOptions: {
-  //     input: glob.sync('./src/*.html')
-  //   },
-  //   outDir: '../dist'
-  // }
 })
